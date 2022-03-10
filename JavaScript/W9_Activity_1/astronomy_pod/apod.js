33fdf8ab825f3b7b70215a3ed981192239df2957
Croteau <rrcroteau@email.neit.edu>
2022-03-10 10:28:09 -0500

    Week9 Activity Complete

---
+++ JavaScript/W9_Activity_1/astronomy_pod/apod.js
@@ -0,0 +1,95 @@
+﻿"use strict";
+
+//returns date string YYYY-MM-DD format
+const getDateString = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+//function to generate the html to display the image or video based on the JSON data passed in
+const displayPicture = data => {
+
+    let html = "";
+    if (data.error) {  //got an error, display it
+        html += `<span class="error">${data.error.message}</span>`
+    }
+    else if (data.code) {  //this means a problem, display it
+        html += `<span class="error">${data.msg}</span>`
+    }
+    else { //success - display the image/video
+        html += `<h3>${data.title}</h3>`;
+        const width = 700;
+        switch (data.media_type) {
+            case "image":
+                html += `<img src="${data.url}" width="${width}" alt="NASA Photo">`;
+                break;
+            case "video":
+                html += `<iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+                break;
+            default:
+                html += `<img src="images/notavailable.png" width="${width}" alt="NASA Photo">`;
+        }
+
+        //date and copyright
+        html += `<div>${data.date}`;
+        if (data.copyright) {
+            html += `<span class="right">&copy; ${data.copyright}</span>`;
+        }
+        html += `</div>`
+
+        //explanation
+        html += `<p>${data.explanation}</p>`;
+    }
+
+    //display the html
+    $("#display").html(html)
+};
+
+//function to display an error, should we get one
+const displayError = error => {
+    let html = `<span class="error">${error.message}</span>`;
+    $("#display").html(html);
+};
+
+//all the actions to take once the page is loaded (ready)
+$(document).ready(() => {
+
+    //get todays date and format it to the required date string the NASA site wants
+    const today = new Date();
+    let dateStr = getDateString(today);
+
+    //set today's date into the text input
+    const dateTextbox = $("#date");
+    dateTextbox.val(dateStr);
+    dateTextbox.focus();
+
+    //set event handler for View button's click event
+    $("#view_button").click(() => {
+
+        //get the date that is in the text box currenty
+        dateStr = $("#date").val();
+        const dateObj = new Date(dateStr);
+
+        //if it is an invalid date, give the user an error
+        if (dateObj == "Invalid Date") {
+            const msg = "Please enter a valid date in YYYY-MM-DD format.";
+            $("#display").html(`<span class="error">${msg}</span>`);
+        }
+
+        else {
+            //put the string into the proper format
+            dateStr = getDateString(dateObj);
+
+            //build a URL for API request
+            const domain = `https://api.nasa.gov/planetary/apod`;  //domain portion of URL
+            const request = `?api_key=DEMO_KEY&date=${dateStr}`; //request portion of URL   
+            const url = domain + request //merge both for the full URL
+
+            //fetch the data we need to display the image/video or the error
+            fetch(url)
+                .then(response => response.json())
+                .then(json => displayPicture(json))
+                .catch(e => displayError(e));
+
+        }
+        $("#date").focus(); //set the cursor in the data field to begin with
+
+    });
+});
---
+++ JavaScript/W9_Activity_2/photo_viewer_updated/photo_viewer.js
@@ -0,0 +1,72 @@
+﻿"use strict";
+
+//'async' makes the function asynchronous and 'await' tells JS to wait until the Promise is settled...then return the result
+const domain = "https://jsonplaceholder.typicode.com";
+
+//asynchronous function to retrieve a photo and related date from various sources
+const getPhoto = async id => {
+
+    //the photos only go from 1-5000, if the input is outside this give the user an error
+    if (id < 1 || id > 5000) {
+
+        return Promise.reject(
+            new Error("Photo ID must be between 1 and 5000"));
+    }
+
+    //ELSE (it is a number 1-5000) access the data across multiple fetches using "await" to help control flow
+    else {
+
+        //get the photo based on id
+        const r1 = await fetch(`${domain}/photos/${id}`);
+        const photo = await r1.json();
+
+        //use the photo's album id to pull the album's info and place it within an album in the photo object
+        const r2 = await fetch(`${domain}/albums/${photo.albumId}`);
+        const album = await r2.json();
+        photo.album = album;
+
+        //now get the user information from the id
+        const r3 = await fetch(`${domain}/users/${photo.album.userId}`);
+        const user = await r3.json();
+        photo.album.user = user;
+
+        //return the filled photo object
+        return photo;
+
+    }
+
+};
+
+//develop the HTML to display photo and data 
+const displayPhotoData = photo => {
+
+    let html = `<img src="${photo.thumbnailUrl}" alt="${photo.title}"`;
+    html += `<h4>Title: ${photo.title}</h4>`;
+    html += `<p>Album: ${photo.album.title}</p>`;
+    html += `Posted by: ${photo.album.user.username}`;
+    $("#photo").html(html);
+
+};
+
+//develop the HTML for an error message
+const displayError = e => {
+    let html = `<span>${e}</span>`;
+    $("#photo").html(html);
+};
+
+$(document).ready(() => {
+
+    //set up the async button event
+    $("#view_button").click(async () => {
+
+        const photo_id = $("#photo_id").val(); //get the photo id from the input
+        try { //use try to protect from crashes... it will go to 'catch' if there is an exception
+            const photo = await getPhoto(photo_id);
+            displayPhotoData(photo);
+        }
+        catch(e) {
+
+            displayError(e); //display the error instead of crashing the page
+        }
+    });
+});
