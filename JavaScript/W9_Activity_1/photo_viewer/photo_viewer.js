6e771dc78bb20c9d034a67d165d18d41c4cbecb8
Ron Croteau <rrcroteau@email.neit.edu>
2022-03-10 20:25:59 -0500

    W9 Lab Complete

---
+++ JavaScript/W9_Activity_1/photo_viewer/photo_viewer.js
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+const domain = "https://jsonplaceholder.typicode.com"; //web service URL (without specific folder)
+
+//function to retrieve photo and its data based on photo ID (free samples run from 1-5000)
+const getPhoto = (id) => {
+    if (id < 1 || id > 5000) { //if the id is out of bounds of the samples (1-5000)
+        return Promise.reject(new Error("Photo ID must be between 1 and 5000"));
+    }
+    else { //esle, fetch the photo based on its id
+        return fetch(`${domain}/photos/${id}`)
+            .then(response => response.json());
+    }
+
+};
+
+//Get a photo album based on a specific photo and its Album ID
+const getPhotoAlbum = photo => {
+    return fetch(`${domain}/albums/${photo.albumId}`)
+        .then(response => response.json()) //resolves to an album object
+        .then(album => {
+            photo.album = album; //add album property
+            return photo; //wraps in a promise object
+        });
+};
+
+//get the user credited for the photo
+const getPhotoAlbumUser = photo => {
+    return fetch(`${domain}/users/${photo.album.userId}`)
+        .then(response => response.json()) //resolves to user object
+        .then(user => {
+            photo.album.user = user; //add album.user object
+            return photo; //wraps in a promise object
+        });
+};
+
+//develop the HTML to display photo and data 
+const displayPhotoData = photo => {
+
+    let html = `<img src="${photo.thumbnailUrl}" alt="${photo.title}"`;
+    html += `<h4>Title: ${photo.title}</h4>`;
+    html += `<p>Album: ${photo.album.title}</p>`;
+    html += `Posted by: ${photo.album.user.username}`;
+    $("#photo").html(html);
+
+};
+
+//develop the HTML for an error message
+const displayError = e => {
+    let html = `<span>${e}</span>`;
+    $("#photo").html(html);
+};
+
+//when the document is loaded and ready, create the button click event to use the id in the text box to pull up the data needed to display the html we created
+$(document).ready(() => {
+
+    $("#view_button").click(() => {
+        const photo_id = $("#photo_id").val();
+        getPhoto(photo_id)
+            .then(getPhotoAlbum)
+            .then(getPhotoAlbumUser)
+            .then(displayPhotoData)
+            .catch(displayError)
+    });
+});
---
+++ JavaScript/W9_Lab/post_viewer.js
@@ -0,0 +1,79 @@
+﻿"use strict";
+
+//'async' makes the function asynchronous and 'await' tells JS to wait until the Promise is settled...then return the result
+const domain = "https://jsonplaceholder.typicode.com";
+
+//asynchronous function to retrieve a post and related date from various sources
+const getPost = async id => {
+
+    //the posts only go from 1-100, if the input is outside this give the user an error
+    if (id < 1 || id > 100) {
+
+        return Promise.reject(
+            new Error("Post ID must be between 1 and 100"));
+    }
+
+    //ELSE (it is a number 1-100) access the data across multiple fetches using "await" to help control flow
+    else {
+
+        //get the post based on id
+        const r1 = await fetch(`${domain}/posts/${id}`);
+        const post = await r1.json();
+
+        //use the post's user id to pull the user's info and place it within a user in the post object
+        const r2 = await fetch(`${domain}/users/${post.userId}`);
+        const user = await r2.json();
+        post.user = user;
+
+        //use the post id to get the list of comments for each post (5 comments per post)
+        const r3 = await fetch(`${domain}/posts/${id}/comments`)
+        const comments = await r3.json();
+        post.comments = comments;
+
+        //return the filled post object
+        return post;
+
+    }
+
+};
+
+
+//develop the HTML to display post and data 
+const displayPostData = post => {
+
+    let html = `<h4>Title: ${post.title}</h4>`;
+    html += `<p>Body: ${post.body}</p>`;
+    html += `Posted by: ${post.user.name} <br />`;
+    html += `Contact: ${post.user.email}`;
+    html += `<br /><p><h4>Comments:</h4>`;
+    for (post.comment of post.comments) {
+        html += `${post.comment.body}<br />`;
+        html += `----${post.comment.email}<br /><br />`
+    }
+    html += `</p>`
+    $("#post").html(html);
+
+};
+
+//develop the HTML for an error message
+const displayError = e => {
+    let html = `<span>${e}</span>`;
+    $("#post").html(html);
+};
+
+$(document).ready(() => {
+
+    //set up the async button event
+    $("#view_button").click(async () => {
+
+        const post_id = $("#post_id").val(); //get the post id from the input
+        try { //use try to protect from crashes... it will go to 'catch' if there is an exception
+            const post = await getPost(post_id);
+            displayPostData(post);
+        }
+        catch(e) {
+
+            displayError(e); //display the error instead of crashing the page
+        }
+    });
+});
